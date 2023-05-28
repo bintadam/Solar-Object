8b4347f3627cc809181f82b94d9c0b6fb26e4064
bintadam <zakiyaadan12@gmail.com>
2023-05-28 15:39:29 +0200

    added eventlistener

--- script.js
+++ script.js
@@ -8,49 +8,63 @@ const uranusGravity = 8.87
 const mercuryGravity = 3.7
 
 const img = document.querySelector(".img")
-const mass = document.getElementById('mass')
+const mass = document.getElementById('mass').value
 const btn = document.querySelector('.btn')
-const noMass = document.querySelector("no-mass")
+const noMass = document.querySelector(".no-mass")
 const noPlanet =  document.querySelector(".no-planet")
+const card =  document.querySelector(".card")
 
 const planet = document.getElementById("planet")
-const value = planet.value;
-const text = planet.options[planet.selectedIndex].text
+
+planet.addEventListener("change", function handleChange(e){
+    e.target.value;
+    value = planet.options[planet.selectedIndex].value;
+    text = planet.options[planet.selectedIndex].text;
+})
 
 let weight = 0;
 btn.addEventListener('click', function(e){
     e.preventDefault()
-    if(mass == ""){
-        noMass.textContent  = "No Mass"
-    }  
-    if(planet.value == "earth"){
+    if(mass == null || mass == ""){
+        card.style.display = "none"
+        noMass.textContent  = "Mass Required"
+    } 
+    if(value == "earth"){
         weight = mass * earthGravity;
+        img.classList.add("card-1");
         img.src = "images/earth.png";
         return weight
-    } else if(planet.value == "jupiter"){
+    } else if(value == "jupiter"){
         weight = mass * jupiterGravity;
+        img.classList.add("card-1");
         img.src = "images/jupiter.png";
         return weight
-    } else if(planet.value == "neptune"){
+    } else if(value == "neptune"){
         weight = mass * neptuneGravity;
+        img.classList.add("card-1");
         img.src = "images/neptune.png";
-    } else if(planet.value == "saturn"){
+    } else if(value == "saturn"){
         weight = mass * saturnGravity;
         img.src = "images/saturn.png";
-    } else if(planet.value == "mercury"){
+        img.classList.add("card-1");
+    } else if(value == "mercury"){
         weight = mass * mercuryGravity;
+        img.classList.add("card-1");
         img.src = "images/mercury.png";
-    } else if (planet.value == "mars"){
+    } else if (value == "mars"){
         weight = mass * marsGravity;
+        img.classList.add("card-1");
         img.src = "images/mars.png";
-    } else if(planet.value == "venus"){
+    } else if(value == "venus"){
         weight = mass * venusGravity;
+        img.classList.add("card-1");
         img.src = "images/venus.png";
-    } else if(planet.value == "uranus"){
-        weight = mass * uranusGravity
+    } else if(value == "uranus"){
+        weight = mass * uranusGravity;
+        img.classList.add("card-1");
         img.src = "images/uranus.png"; 
     } else{
-        noPlanet.textContent = "You did not select any Planet"
+        card.style.display = "none"
     }
-    return weight      
+    return alert(weight)      
 })
