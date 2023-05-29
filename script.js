be5ec030e4244193190d37aaf2b03b7af644c281
bintadam <zakiyaadan12@gmail.com>
2023-05-29 19:15:09 +0200

    made some changes

--- script.js
+++ script.js
@@ -1,70 +1,89 @@
-const earthGravity =  9.807
-const jupiterGravity = 24.79
-const saturnGravity = 10.44
-const marsGravity = 3.71
-const venusGravity = 8.87
-const neptuneGravity = 11.15
-const uranusGravity = 8.87
-const mercuryGravity = 3.7
+const btn = document.querySelector('.btn');
+const noMass = document.querySelector(".no-mass");
+const noPlanet =  document.querySelector(".no-planet");
+const card =  document.querySelector(".card");
+const object = document.querySelector(".object")
 
-const img = document.querySelector(".img")
-const mass = document.getElementById('mass').value
-const btn = document.querySelector('.btn')
-const noMass = document.querySelector(".no-mass")
-const noPlanet =  document.querySelector(".no-planet")
-const card =  document.querySelector(".card")
 
-const planet = document.getElementById("planet")
+let weight;
 
-planet.addEventListener("change", function handleChange(e){
-    e.target.value;
-    value = planet.options[planet.selectedIndex].value;
-    text = planet.options[planet.selectedIndex].text;
-})
+function calculate(){
+    const planet = document.getElementById("planet").value;
+    const mass = Number(document.getElementById('mass').value);
+    const img = document.querySelector(".img");
+    console.log(mass)
+    console.log(typeof mass)
 
-let weight = 0;
-btn.addEventListener('click', function(e){
-    e.preventDefault()
-    if(mass == null || mass == ""){
-        card.style.display = "none"
-        noMass.textContent  = "Mass Required"
+    let earthGravity =  9.807
+    let jupiterGravity = 24.79
+    let saturnGravity = 10.44
+    let marsGravity = 3.71
+    let venusGravity = 8.87
+    let neptuneGravity = 11.15
+    let uranusGravity = 8.87
+    let mercuryGravity = 3.7
+        
+    if(!mass){
+        img.style.display = "none"
+        noMass.textContent = "Mass is Required";
+        object.textContent = ""
     } 
-    if(value == "earth"){
-        weight = mass * earthGravity;
-        img.classList.add("card-1");
-        img.src = "images/earth.png";
-        return weight
-    } else if(value == "jupiter"){
-        weight = mass * jupiterGravity;
-        img.classList.add("card-1");
-        img.src = "images/jupiter.png";
-        return weight
-    } else if(value == "neptune"){
-        weight = mass * neptuneGravity;
-        img.classList.add("card-1");
-        img.src = "images/neptune.png";
-    } else if(value == "saturn"){
-        weight = mass * saturnGravity;
-        img.src = "images/saturn.png";
-        img.classList.add("card-1");
-    } else if(value == "mercury"){
-        weight = mass * mercuryGravity;
-        img.classList.add("card-1");
-        img.src = "images/mercury.png";
-    } else if (value == "mars"){
-        weight = mass * marsGravity;
-        img.classList.add("card-1");
-        img.src = "images/mars.png";
-    } else if(value == "venus"){
-        weight = mass * venusGravity;
-        img.classList.add("card-1");
-        img.src = "images/venus.png";
-    } else if(value == "uranus"){
-        weight = mass * uranusGravity;
-        img.classList.add("card-1");
-        img.src = "images/uranus.png"; 
-    } else{
-        card.style.display = "none"
+
+    switch(planet){
+        case "Earth":
+            weight = mass * earthGravity;
+            img.src = "images/earth.png";
+            console.log(weight)
+            break;
+
+        case "Jupiter":
+            weight = mass * jupiterGravity;
+            img.src = "images/jupiter.png";
+            break;
+
+        case "Neptune":
+            weight = mass * neptuneGravity;
+            img.src = "images/neptune.png";
+            break;
+
+        case "Saturn":
+            weight = mass * saturnGravity;
+            img.src = "images/saturn.png";
+            break;
+
+        case "Mercury":
+            weight = mass * mercuryGravity;
+            img.src = "images/mercury.png";
+            break;
+
+        case "Mars":
+            weight = mass * marsGravity;
+            img.src = "images/mars.png";
+            break;
+
+        case "Menus":
+            weight = mass * venusGravity;
+            img.src = "images/venus.png";
+            break;
+
+        case "Uranus":
+            weight = mass * uranusGravity;
+            img.src = "images/uranus.png"; 
+            break;
+            
+        default :
+            noMass.textContent = "You did not choose a planet yet";
+            img.style.display = "none"
+            object.textContent = ""
     }
-    return alert(weight)      
+
+
+}
+
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+
+    calculate()
 })
+
+
